32474a1d2d227387e11bb3ce733e8b4d2796f6e6
Taha <144627369+Tahanoa@users.noreply.github.com>
2025-11-17 18:50:12 +0330

    create project

---
+++ src/main/java/org/example/tblog/TBlogApplication.java
@@ -0,0 +1,13 @@
+package org.example.tblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tblog/TBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

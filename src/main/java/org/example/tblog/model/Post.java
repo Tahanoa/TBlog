eae9dd22003f067b5ba2fa3e5e554a94d018ac22
Taha <144627369+Tahanoa@users.noreply.github.com>
2025-11-19 00:36:55 +0330

    add security & Users primary

--- src/main/java/org/example/tblog/model/Post.java
+++ src/main/java/org/example/tblog/model/Post.java
@@ -1,9 +1,4 @@
 package org.example.tblog.model;
 
 public class Post {
-    private int id;
-    private String title;
-    private String content;
-    private String author;
-    private Category category;
 }
--- src/main/java/org/example/tblog/model/Tag.java
+++ src/main/java/org/example/tblog/model/Tag.java
@@ -1,39 +1,4 @@
 package org.example.tblog.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
 public class Tag {
-    private int id;
-    private String name;
-    private String slug;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSlug() {
-        return slug;
-    }
-
-    public void setSlug(String slug) {
-        this.slug = slug;
-    }
 }

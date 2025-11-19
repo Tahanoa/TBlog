8a6c79fee338ad54c7f7cd1712eea26635848cc9
Taha <144627369+Tahanoa@users.noreply.github.com>
2025-11-19 23:12:33 +0330

    edit: CommentsStatus

--- src/main/java/org/example/tblog/repository/CommentRepository.java
+++ src/main/java/org/example/tblog/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package org.example.tblog.repository;
 
 import org.example.tblog.model.Comment;
+import org.example.tblog.model.CommentsStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
     Page<Comment> findByPostIdOrderByTimeDesc(Integer postId , Pageable pageable);
-
+    Page<Comment> findByPostIdAndStatus(Integer postId, CommentsStatus status, Pageable pageable);
     Long countByPostId(Integer postId);
 }
